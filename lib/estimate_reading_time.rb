04733751cc49a0f9e7600b708124c0182aaed4fe
nce27 <nc3elliott@gmail.com>
2023-05-15 10:28:50 +0100

    green test for challenge 1. Outpus 1 if text is 200 words, 0 if empty.

--- lib/estimate_reading_time.rb
+++ lib/estimate_reading_time.rb
@@ -1,6 +1,8 @@
 def estimate_reading_time(text)
-    words_per_minute = 200
-    words_count = text.split.size
-    (words_count.to_f / words_per_minute).ceil
+    words = text.split(" ")
+    return (words.length / 200.to_f).ceil
+    # words_per_minute = 200
+    # words_count = text.split.size
+    # (words_count.to_f / words_per_minute).ceil
   end
   
--- spec/estimate_reading_time_spec.rb
+++ spec/estimate_reading_time_spec.rb
@@ -12,23 +12,23 @@ RSpec.describe "estimate_reading_time" do
     end 
 
     it "estimates a result for a text of 200 words return 1" do 
-        result = estimate_reading_time("Two " * 200)
+        result = estimate_reading_time("one " * 200)
         expect(result).to eq 1
     end 
 
     it "estimates a result for a text of 300 words return 2" do 
-        result = estimate_reading_time("Three " * 300)
+        result = estimate_reading_time("one " * 300)
         expect(result).to eq 2
     end 
 
     it "estimates a result for a text of 400 words return 2" do 
-        result = estimate_reading_time("Four " * 400)
+        result = estimate_reading_time("one " * 400)
         expect(result).to eq 2
     end 
 
-    it "estimates a result for a text of 20,000 words return 100" do 
-        result = estimate_reading_time("Twenty_thousand " * 20000)
-        expect(result).to eq 100
+    it "estimates a result for a text of 5,000 words return 100" do 
+        result = estimate_reading_time("one " * 5000)
+        expect(result).to eq 25
     end 
 end
 
