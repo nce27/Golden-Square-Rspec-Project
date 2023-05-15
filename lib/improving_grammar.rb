28019a77ac26b1f888cf2f820708abbc19573056
nce27 <nc3elliott@gmail.com>
2023-05-15 13:59:07 +0100

    Uncommented StringQualityException.new line so that the raise error parentheses on line 24 works.

--- lib/improving_grammar.rb
+++ lib/improving_grammar.rb
@@ -5,11 +5,9 @@ def improving_grammar(text)
 
   #   raise StringQualityException.new("String must end with full stop!")
 
-  # elsif text[0] != text[0].upcase
-  #   raise StringQualityException.new("String must begin with capital letter!")
-
    elsif text[0] != text[0].upcase
-      return false
+      #return false
+      raise StringQualityException.new("String must begin with capital letter!")
   else
     return true
   end
