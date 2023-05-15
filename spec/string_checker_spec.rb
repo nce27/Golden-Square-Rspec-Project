b649328e6327df04baabed01498fad0767366589
nce27 <nc3elliott@gmail.com>
2023-05-15 16:48:43 +0100

    Updated test to return true if passed the string #TODO

--- spec/string_checker_spec.rb
+++ spec/string_checker_spec.rb
@@ -6,11 +6,18 @@ RSpec.describe "string_checker method" do
       expect { string_checker("")}.to raise_error("fail, gave an empty string")
     end
   end
-  
+
   context "when given a string that contains TODO without '#'" do
     it "fails" do
       result = string_checker("TODO")
       expect(result).to eq false
+    end
+  end
+  
+  context "when given a string that contains #TODO" do
+    it "passes" do
+      result = string_checker("#TODO")
+      expect(result).to eq true
     end
   end  
 end
