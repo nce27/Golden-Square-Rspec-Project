48bc7132364067e1e9a232b35357e5b27ff98f0d
nce27 <nc3elliott@gmail.com>
2023-05-15 11:47:13 +0100

    Improved code to return true if the string begins with a captial letter and ends with a full stop.

--- lib/improving_grammar.rb
+++ lib/improving_grammar.rb
@@ -1,11 +1,13 @@
 def improving_grammar(text)
   if text.empty?
     raise EmptyStringException.new("Cannot give empty string!")
+  else
+    return true
   end
 end
 
 class EmptyStringException < Exception
-  def initialize(message)
+  def initialize(message) 
     super("EmptyStringException occured: #{message}")
   end
 end
--- spec/improving_grammar_spec.rb
+++ spec/improving_grammar_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe do
         expect { improving_grammar("") }.to raise_error(EmptyStringException, "EmptyStringException occured: Cannot give empty string!")
     end
   end
+  context "given a sentence that begins with a captial and full stop" do
+    it "returns true" do
+      result = improving_grammar("Hello there.")
+      expect(result).to eq true
+    end
+  end
+
 end
